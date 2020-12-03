4051824209424cd2541e3669fc3be068e6f35dcc
Joe Fredette <jfredett@gmail.com>
2020-12-03 09:39:37 -0500

    [day3] "part 2 complete"

--- solutions/day3.rb
+++ solutions/day3.rb
@@ -17,17 +17,17 @@ class Map < TaskData
   end
 
   def [](x,y)
-    raise "Off the map" if y >= self.height
     @map[y][x % self.width]
   end
 
   def ride!(toboggan)
     trees = 0
-    while toboggan.position[1] != self.height
+    while toboggan.position[1] < self.height
       trees += 1 if self.tree_at?(toboggan)
       toboggan.slide!
     end
-    return trees
+
+    toboggan.performance = trees
   end
 
   def tree_at?(toboggan)
@@ -38,6 +38,8 @@ end
 
 class Toboggan
   attr_reader :run, :fall, :position
+
+  attr_accessor :performance
 
   def initialize(run, fall)
     @position = [0,0]
@@ -51,7 +53,28 @@ class Toboggan
 end
 
 map = Map.new('day3')
-toboggan = Toboggan.new(3,1)
 
-puts "Part 1 | Trees hit: #{map.ride!(toboggan)}"
+toboggans = [
+  Toboggan.new(1,1),
+  Toboggan.new(3,1),
+  Toboggan.new(5,1),
+  Toboggan.new(7,1),
+  Toboggan.new(1,2)
+]
+
+
+toboggans.each do |toboggan|
+  map.ride!(toboggan)
+end
+
+part1_toboggan = toboggans[1]
+
+puts map.height
+
+puts "Part 1 | Trees hit: #{part1_toboggan.performance}"
+puts "Part 2 | Product of Trees hit: #{toboggans.map(&:performance).reduce(&:*)}"
+
+
+
+
 
