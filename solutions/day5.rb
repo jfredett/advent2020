38830d0190bf8f7946e07ec1bcb6c57897bbb3aa
Joe Fredette <jfredett@gmail.com>
2020-12-05 00:31:49 -0500

    [day5] "part 2 complete"

--- solutions/day5.rb
+++ solutions/day5.rb
@@ -59,4 +59,13 @@ end
 
 puts "Test 1 | 'FBFBBFFRLR' => #{BoardingCode.new('FBFBBFFRLR').seat_id}"
 
-puts "Part 1 | Highest Seat ID #{data.map(&:seat_id).max}"
+puts "Part 1 | Highest Seat ID: #{data.map(&:seat_id).max}"
+
+seats = data.map(&:seat_id).sort
+
+my_seat =  seats.each.with_index do |s, i|
+  next if seats[i+1] - s == 1
+  break s + 1
+end
+
+puts "Part 2 | My Seat: #{my_seat}"
