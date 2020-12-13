c52b90da1098275ceb4e0dbb140e94f8c74cb4df
Joe Fredette <jfredett@gmail.com>
2020-12-13 11:28:15 -0500

    [day13] "part 2 complete
    totally cheated and used mathematica for the CRT implementation, but CRT is my nemesis so I do not care."

--- solutions/day13.rb
+++ solutions/day13.rb
@@ -28,17 +28,30 @@ class Schedule
       current_time += 1
     end
   end
+
+  def contest_answer
+    # Chinese Remainder Theorem of Doooom, this just prints out the coefficients
+    # and tells you to drop it in mathematica.
+
+    coefs = @buses.map(&:remainder_coefficients)
+    "ChineseRemainder[\{#{coefs.map { |e| e[0] }.join(",")}\}, \{#{coefs.map { |e| e[1] }.join(",")}\}]"
+  end
 end
 
 class Bus
-  attr_reader :id
+  attr_reader :id, :offset
 
-  def initialize(id)
+  def initialize(id, offset)
     @id = id
+    @offset = offset
   end
 
   def present?(timestamp)
     timestamp % @id == 0
+  end
+
+  def remainder_coefficients
+    [id - offset, id]
   end
 end
 
@@ -47,7 +60,13 @@ Result.output for: 'day13' do
     raw_timestamp, bus_id_array = data.split("\n")
     timestamp = raw_timestamp.to_i
 
-    buses = bus_id_array.chomp.split(',').reject { |b_id| b_id == "x" }.map { |b_id| Bus.new(b_id.to_i) }
+    buses = []
+    offset = -1
+    bus_id_array.chomp.split(',').each do |b_id|
+      offset += 1
+      next if b_id == "x"
+      buses << Bus.new(b_id.to_i, offset)
+    end
 
     Schedule.new(timestamp, buses)
   end
@@ -56,4 +75,79 @@ Result.output for: 'day13' do
     bus, timestamp = data[0].find_earliest_bus
     (timestamp - data[0].timestamp) * bus.id
   end
+
+  part 2, "Chinese Remainder Buses" do
+    "Pop this into Mathematica: #{data[0].contest_answer}"
+  end
+
 end
+
+__END__
+
+
+Chinese Remainder Theorem Time. I hated this in Number Theory and I still hate it today.
+
+    7a = t
+    13b = t + 1
+    59c = t + 4
+    31d = t + 6
+    19e = t + 7
+
+Phrase this as:
+
+
+    t = 7a
+    t = 13b - 1
+    t = 59c - 4
+    t = 31d - 6
+    t = 19e - 7
+
+Then
+
+    t = 0 mod 7
+    t = 12 mod 13
+    t = 55 mod 59
+    t = 25 mod 31
+    t = 12 mod 19
+
+CRT gives you back the right answer.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
